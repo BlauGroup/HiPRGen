eaa03471112d4d2962d41dcbc7048c6c763b7a20
Daniel Barter <danielbarter@gmail.com>
2021-06-03 12:34:38 -0700

    ..

--- species_filter.py
+++ species_filter.py
@@ -3,15 +3,15 @@ from mol_entry import MoleculeEntry
 
 """
 Phase 1: species filtering
-input: a list of species
-output: a filtered list of species with fixed indices
-description: this is where we remove isomorphic species, and do other forms of filtering that we haven't thought of yet. We can organize things in such a way that adding more filters doesn't require someone to modify the inner loop
+input: a list of dataset entries
+output: a filtered list of mol_entries with fixed indices
+description: this is where we remove isomorphic species, and do other forms of filtering. species_filter_decision_tree contains the filters
 """
 
-def species_filter(dataset_entries):
+def species_filter(dataset_entries, species_filter_decision_tree):
     mol_entries = [ MoleculeEntry.from_dataset_entry(e) for e in dataset_entries ]
 
-    # filters go here
+    # TODO implement filtering
 
     for i, e in enumerate(mol_entries):
         e.parameters["ind"] = i
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from monty.serialization import loadfn, dumpfn
 from species_filter import *
 
+class bcolors:
+    PASS = '\u001b[32;1m'
+    FAIL = '\u001b[31;1m'
+    ENDC = '\u001b[0m'
+
+
 
 def test_species_filter(ronald_LIBE):
-    ronald_mol_entries = species_filter(ronald_LIBE)
+    ronald_mol_entries = species_filter(ronald_LIBE, None)
     ronald_mol_entries_from_disk = loadfn("./data/ronald_mol_entries.json")
     if ronald_mol_entries == ronald_mol_entries_from_disk:
-        print("passed: test_species_filter")
+        print(bcolors.PASS + "passed: test_species_filter" + bcolors.ENDC)
     else:
-        print("failed: test_species_filter")
+        print(bcolors.FAIL + "failed: test_species_filter" + bcolors.ENDC)
         quit()
 
     return ronald_mol_entries
