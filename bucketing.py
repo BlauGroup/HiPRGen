b2ce1ac037ab7ff2f299ac0bf96eec4f41c27901
Daniel Barter <danielbarter@gmail.com>
2021-06-03 13:32:30 -0700

    preliminary bucketing finished

--- bucketing.py
+++ bucketing.py
@@ -1,5 +1,6 @@
 from mol_entry import MoleculeEntry
 from itertools import combinations_with_replacement
+import sqlite3
 
 """
 Phase 2: bucketing pairs of species
@@ -8,7 +9,48 @@ output: buckets labeled by atom count containing individual species and pairs of
 description: since each reaction conserves atom numbers, a concerted reaction only occurs between elements in a single bucket. There are tricks to reduce the number of pairs (like don't include (A,B) and (B,A)). If the number of species is 10,000, there are only 100 million such pairs which is within reach
 """
 
-def bucket(mol_entries, bucket_folder):
-    for (m1, m2) in combinations_with_replacement(mol_entries, 2):
-        tag = '_'.join(sorted(m1.species + m2.species))
-        print(tag)
+def create_table_sql(tag):
+    return "CREATE TABLE " + tag + " (species_1, species_2)"
+
+def insert_complex_sql(tag):
+    return "INSERT INTO " + tag + " VALUES (?, ?)"
+
+class Bucket:
+
+    def insert_complex(self, tag, inds):
+        cur = self.con.cursor()
+
+
+        if tag not in self.insert_statements:
+            cur.execute(create_table_sql(tag))
+            self.insert_statements[tag] = insert_complex_sql(tag)
+
+        cur.execute(self.insert_statements[tag], inds)
+
+    def __init__(self, mol_entries, bucket_db, commit_freq=1000):
+        self.con = sqlite3.connect(bucket_db)
+        self.insert_statements = {}
+
+
+        count = 0
+        for m in mol_entries:
+            tag = '_'.join(sorted(m.species))
+            inds = (m.parameters["ind"], -1)
+            self.insert_complex(tag, inds)
+
+            count += 1
+            if count % commit_freq == 0:
+                self.con.commit()
+
+        for (m1, m2) in combinations_with_replacement(mol_entries, 2):
+            tag = '_'.join(sorted(m1.species + m2.species))
+            inds = (m1.parameters["ind"], m2.parameters["ind"])
+            self.insert_complex(tag, inds)
+
+            count += 1
+            if count % commit_freq == 0:
+                self.con.commit()
+
+        self.con.commit()
+        self.con.close()
+
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from monty.serialization import loadfn, dumpfn
 from species_filter import *
+from bucketing import *
 
 class bcolors:
     PASS = '\u001b[32;1m'
@@ -11,16 +12,20 @@ class bcolors:
 def test_species_filter(ronald_LIBE):
     ronald_mol_entries = species_filter(ronald_LIBE, None)
     ronald_mol_entries_from_disk = loadfn("./data/ronald_mol_entries.json")
+
+
     if ronald_mol_entries == ronald_mol_entries_from_disk:
         print(bcolors.PASS + "passed: test_species_filter" + bcolors.ENDC)
+        return ronald_mol_entries
     else:
         print(bcolors.FAIL + "failed: test_species_filter" + bcolors.ENDC)
         quit()
 
-    return ronald_mol_entries
-
+def test_bucketing(mol_entries):
+    b = Bucket(mol_entries, './scratch/buckets.sqlite')
 
 
 ronald_LIBE = loadfn("./data/ronald_LIBE.json")
 
 ronald_mol_entries = test_species_filter(ronald_LIBE)
+test_bucketing(ronald_mol_entries)
