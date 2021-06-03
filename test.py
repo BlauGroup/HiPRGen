3a1b4126c3c8221524702f387a5ec50c645656cb
Daniel Barter <danielbarter@gmail.com>
2021-06-03 12:25:26 -0700

    start of test chain

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ from monty.serialization import loadfn, dumpfn
 from species_filter import *
 
 
-def test_species_filter():
-    ronald_LIBE = loadfn("./data/ronald_LIBE.json")
+def test_species_filter(ronald_LIBE):
     ronald_mol_entries = species_filter(ronald_LIBE)
     ronald_mol_entries_from_disk = loadfn("./data/ronald_mol_entries.json")
-    return ronald_mol_entries == ronald_mol_entries_from_disk
+    if ronald_mol_entries == ronald_mol_entries_from_disk:
+        print("passed: test_species_filter")
+    else:
+        print("failed: test_species_filter")
+        quit()
+
+    return ronald_mol_entries
 
 
-if test_species_filter():
-    print("passed: test_species_filter")
-else:
-    print("failed: test_species_filter")
-    quit()
+
+ronald_LIBE = loadfn("./data/ronald_LIBE.json")
+
+ronald_mol_entries = test_species_filter(ronald_LIBE)
