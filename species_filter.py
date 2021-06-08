533fdd08ddd756b4152f5191400181dff7b2eac6
Daniel Barter <danielbarter@gmail.com>
2021-06-08 08:34:33 -0700

    ..

--- species_filter.py
+++ species_filter.py
@@ -1,5 +1,5 @@
 from mol_entry import MoleculeEntry
-
+from enum import Enum
 
 """
 Phase 1: species filtering
@@ -10,6 +10,8 @@ description: this is where we remove isomorphic species, and do other forms of f
 species decision tree
 
 A question is a function q(mol_entry) -> Bool
+
+Unlike for reaction filtering, these questions should not modify the mol_entry in any way.
 
 A node is either a Terminal or a non empty list [(question, node)]
 
@@ -25,10 +27,44 @@ Once a Terminal node is reached, it tells us whether to keep or discard the spec
 
 """
 
-def species_filter(dataset_entries, species_decision_tree):
-    mol_entries = [ MoleculeEntry.from_dataset_entry(e) for e in dataset_entries ]
+class Terminal(Enum):
+    KEEP = 1
+    DISCARD = -1
 
-    # TODO implement filtering
+def run_decision_tree(mol_entry, decision_tree):
+    node = decision_tree
+
+    while type(node) == list:
+        next_node = None
+        for (question, new_node) in node:
+            if question(mol_entry):
+                next_node = new_node
+                break
+
+        node = next_node
+
+
+    if type(node) == Terminal:
+        if node == Terminal.KEEP:
+            return True
+        else:
+            return False
+    else:
+        print(node)
+        raise Exception("unexpected node type reached")
+
+
+default_decision_tree = Terminal.KEEP
+
+def species_filter(dataset_entries, species_decision_tree):
+    mol_entries_unfiltered = [
+        MoleculeEntry.from_dataset_entry(e) for e in dataset_entries ]
+
+    mol_entries = [
+        m for m in mol_entries_unfiltered
+        if run_decision_tree(m, default_decision_tree)]
+
+
 
     for i, e in enumerate(mol_entries):
         e.parameters["ind"] = i
