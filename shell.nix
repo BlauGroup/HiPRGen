191ed69f94d9b4a25b57ae904434b4a00c469b14
Daniel Barter <danielbarter@gmail.com>
2021-06-24 13:26:40 -0700

    ..

--- shell.nix
+++ shell.nix
@@ -2,26 +2,7 @@ with (import <nixpkgs> {});
 
 
 let
-  python = let
-    packageOverrides = self: super: {
-
-
-
-      pymatgen = super.pymatgen.overridePythonAttrs (
-        old: { version = "2022.0.6";
-               src = super.fetchPypi {
-                 pname = "pymatgen";
-                 version = "2022.0.4";
-                 extension = "tar.gz";
-                 sha256 = "0x05glcgczyjrmfrigsrm6nb7d9whfk6ilralqwn17hp2264jhbj";};
-               checkInputs = [ super.pytest ];
-             }
-      );
-
-
-    }; in python38.override {inherit packageOverrides;};
-
-  pythonEnv = python.withPackages (
+  pythonEnv = python38.withPackages (
       ps: [ ps.pymatgen
             ps.monty
             ps.openbabel-bindings
