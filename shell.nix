bb099c4d63579e7ee0b0ec2de6f981cb62d802fe
Daniel Barter <danielbarter@gmail.com>
2021-07-01 09:04:54 -0700

    added mip nix expression

--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 with (import <nixpkgs> {});
 
 
+
 let
+  mip = ps: ps.callPackage ./mip.nix {};
+
   pythonEnv = python38.withPackages (
       ps: [ ps.pymatgen
             ps.monty
             ps.openbabel-bindings
             ps.pygraphviz
             ps.mpi4py
+            (mip ps)
           ]);
 in mkShell rec {
   buildInputs = [ pythonEnv
