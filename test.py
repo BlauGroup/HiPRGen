31f6ddfb8af06d291a22ee9f83700b0b93d7df1b
Daniel Barter <danielbarter@gmail.com>
2021-07-09 10:54:42 -0700

    toggling report gen in tests

--- test.py
+++ test.py
@@ -53,6 +53,6 @@ def test_mc_pathfinding(network_loader):
     )
 
 
+test_mc_pathfinding(network_loader)
 test_species_filter(network_loader)
 test_reaction_gen(network_loader)
-# test_mc_pathfinding(network_loader)
