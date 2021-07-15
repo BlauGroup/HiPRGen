cfcde3d78f5e69c37c3384bb4d3a1d60a56bd8a4
Daniel Barter <danielbarter@gmail.com>
2021-07-15 15:04:07 -0700

    updating RNMC

--- RNMC.nix
+++ RNMC.nix
@@ -2,12 +2,12 @@ with (import <nixpkgs> {});
 
 stdenv.mkDerivation rec {
   name = "RNMC";
-  hash = "cd2f73c05818508347d3ab5ba2afc18510ff2c84";
+  hash = "5d9672167e439f04ff3b67aa267aba146b31f86b";
   buildInputs = [gsl];
   nativeBuildInputs = [meson ninja clang sqlite];
   src = fetchurl {
     url = "https://github.com/BlauGroup/RNMC/archive/${hash}.tar.gz";
-    sha256 = "0xy7azk6widznzlipcz8rn55slkszyl8x17fwf45g0pxfvrgvmnv";
+    sha256 = "04x16z4fmx1651z1g9qf39ab9mf443jhhr1qsjdfn3fj0j2nn8gq";
   };
   mesonBuildType = "debug";
 }
