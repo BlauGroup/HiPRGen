375bdbaff7c3510e997ce5a45b7d085b1cce95f6
Daniel Barter <danielbarter@gmail.com>
2021-07-16 11:01:54 -0700

    updating RNMC nix expression

--- RNMC.nix
+++ RNMC.nix
@@ -2,13 +2,14 @@ with (import <nixpkgs> {});
 
 stdenv.mkDerivation rec {
   name = "RNMC";
-  hash = "5d9672167e439f04ff3b67aa267aba146b31f86b";
-  buildInputs = [gsl];
-  nativeBuildInputs = [meson ninja clang sqlite];
+  hash = "b79436dc27feff194e485de649131784d7a6931c";
+  buildInputs = [gsl clang sqlite];
   src = fetchurl {
     url = "https://github.com/BlauGroup/RNMC/archive/${hash}.tar.gz";
-    sha256 = "04x16z4fmx1651z1g9qf39ab9mf443jhhr1qsjdfn3fj0j2nn8gq";
+    sha256 = "17md2pjsxlwg3cd5blr2fzjr0x369z9xsza4imyn7g6l67x8ym9g";
   };
-  mesonBuildType = "debug";
+  buildPhase = "CC=clang ./build.sh";
+  installPhase = "mkdir $out && mkdir $out/bin && mv RNMC $out/bin";
+
 }
 
