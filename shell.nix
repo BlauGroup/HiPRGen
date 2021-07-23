f49eac6671556043000894c500c118f2875ae576
Daniel Barter <danielbarter@gmail.com>
2021-07-23 12:29:19 -0700

    trying plasma

--- shell.nix
+++ shell.nix
@@ -1,13 +1,21 @@
 with (import <nixpkgs> {});
-
-
 let
-  pythonEnv = python38.withPackages (
+  python = let
+    packageOverrides = self: super: {
+      pyarrow = super.pyarrow.overridePythonAttrs (
+        old: { PYARROW_WITH_PLASMA = true;}
+      );
+
+
+    }; in python38.override {inherit packageOverrides;};
+
+  pythonEnv = python.withPackages (
       ps: [ ps.pymatgen
             ps.monty
             ps.openbabel-bindings
             ps.pygraphviz
             ps.mpi4py
+            ps.pyarrow
           ]);
 in mkShell rec {
   buildInputs = [ pythonEnv
