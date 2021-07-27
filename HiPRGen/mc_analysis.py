6d746e5a2b386d1cd2d5165edd58e4c85efdbbf6
Daniel Barter <danielbarter@gmail.com>
2021-07-27 07:12:56 -0700

    added sink report

--- HiPRGen/mc_analysis.py
+++ HiPRGen/mc_analysis.py
@@ -211,3 +211,54 @@ class Pathfinding:
                 break
 
         report_generator.finished()
+
+def sink_report(
+        network_loader,
+        sink_report_path
+):
+
+    consumed_dict = [0] * network_loader.number_of_species
+    produced_dict = [0] * network_loader.number_of_species
+    for seed in network_loader.trajectories:
+        for step in network_loader.trajectories[seed]:
+            reaction_index = network_loader.trajectories[seed][step][0]
+
+            reaction = network_loader.index_to_reaction(reaction_index)
+
+            for i in range(reaction['number_of_reactants']):
+                reactant_index = reaction['reactants'][i]
+                consumed_dict[reactant_index] += 1
+
+            for j in range(reaction['number_of_products']):
+                product_index = reaction['products'][j]
+                produced_dict[product_index] += 1
+
+    max_ratio = 1e10
+    ratio_dict = [max_ratio] * network_loader.number_of_species
+
+    for i in range(network_loader.number_of_species):
+        if consumed_dict[i] != 0:
+            ratio_dict[i] = produced_dict[i] / consumed_dict[i]
+
+
+    sink_data = sorted(
+        list(enumerate(zip(consumed_dict, produced_dict, ratio_dict))),
+        key=lambda item: -item[1][2])
+
+
+
+    report_generator = ReportGenerator(
+        network_loader.mol_entries,
+        sink_report_path,
+        rebuild_mol_pictures=False)
+
+    for species_index, (c,p,r) in sink_data:
+        if c + p > 0:
+            report_generator.emit_text("ratio: " + str(r))
+            report_generator.emit_text("produced: " + str(p))
+            report_generator.emit_text("consumed: " + str(c))
+            report_generator.emit_molecule(species_index)
+            report_generator.emit_newline()
+
+
+    report_generator.finished()
--- test.py
+++ test.py
@@ -61,6 +61,7 @@ def test_mc_pathfinding(network_loader):
     )
 
     species_report(network_loader, './scratch/species_report.tex')
+    sink_report(network_loader, './scratch/sink_report.tex')
 
 
 test_mc_pathfinding(network_loader)
