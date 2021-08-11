596c4b8292273a399d11a51e4cdd9e6e79846d2e
Daniel Barter <danielbarter@gmail.com>
2021-08-10 17:04:21 -0700

    redesigning the testing infrastructure

--- RNMC.nix
+++ RNMC.nix
@@ -2,11 +2,11 @@ with (import <nixpkgs> {});
 
 stdenv.mkDerivation rec {
   name = "RNMC";
-  hash = "1f948bc009696f9b06d27fb60b994b1e94f1edc0";
+  hash = "c770edb9606f07c26438e61936a375a686c2994c";
   buildInputs = [gsl gcc sqlite];
   src = fetchurl {
     url = "https://github.com/BlauGroup/RNMC/archive/${hash}.tar.gz";
-    sha256 = "15d67s1g2aj5ln4gcbd45lv5s88djgi5rrkm0bvslv9dsdjfbxjb";
+    sha256 = "05qn295s9kifkwr71a2rgf7y80kyrc9qli9giazav1i5dbfnk5sy";
   };
   buildPhase = "CC=gcc ./build.sh";
   installPhase = "mkdir $out && mkdir $out/bin && mv RNMC $out/bin";
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import os
+import subprocess
 import sqlite3
 import pickle
 from HiPRGen.mc_analysis import *
 from HiPRGen.network_loader import *
 from HiPRGen.initial_state import find_mol_entry_from_xyz_and_charge
+from monty.serialization import loadfn
+from HiPRGen.species_filter import *
+from HiPRGen.bucketing import *
+from HiPRGen.report_generator import *
+from HiPRGen.initial_state import *
 
 
 class bcolors:
@@ -11,41 +17,77 @@ class bcolors:
     FAIL = '\u001b[31;1m'
     ENDC = '\u001b[0m'
 
-network_loader = NetworkLoader(
-    './scratch/rn.sqlite',
-    './scratch/ronald_mol_entries.pickle'
-    )
+if os.path.isdir('./scratch'):
+    subprocess.run(['rm', '-r', './scratch'])
 
-
-def test_species_filter(network_loader):
-    with open('./data/ronald_mol_entries.pickle', 'rb') as f:
-        mol_entries_data = pickle.load(f)
+subprocess.run(['mkdir', './scratch'])
+subprocess.run(['mkdir', './scratch/li_test' ])
 
 
 
-    if network_loader.mol_entries == mol_entries_data:
-        print(bcolors.PASS + "passed: test_species_filter" + bcolors.ENDC)
-    else:
-        print(bcolors.FAIL + "failed: test_species_filter" + bcolors.ENDC)
+def li_test():
+    folder = './scratch/li_test'
+    database_entries = loadfn('./data/ronald_LIBE.json')
+    mol_entries = species_filter(
+        database_entries,
+        folder + '/ronald_mol_entries.pickle',
+        folder + '/report_file')
 
-def test_reaction_gen(network_loader):
+    bucket(mol_entries, folder + '/buckets.sqlite')
 
-    if network_loader.number_of_reactions == 5525:
-        print(bcolors.PASS + "passed: test_reaction_gen" + bcolors.ENDC)
-    else:
-        print(bcolors.FAIL + "failed: test_reaction_gen" + bcolors.ENDC)
+    subprocess.run([
+        'mpiexec',
+        '--use-hwthread-cpus',
+        '-n',
+        '8',
+        'python',
+        'run_network_generation.py',
+        folder + '/ronald_mol_entries.pickle',
+        folder + '/buckets.sqlite',
+        folder + '/rn.sqlite',
+        folder + '/reaction_report.tex'])
 
 
-def test_mc_pathfinding(network_loader):
+    Li_plus_id = find_mol_entry_from_xyz_and_charge(
+        mol_entries,
+        './xyz_files/Li.xyz',
+        1)
+
+    EC_id = find_mol_entry_from_xyz_and_charge(
+        mol_entries,
+        './xyz_files/EC.xyz',
+        0)
+
+    initial_state = {
+        Li_plus_id : 30,
+        EC_id : 30
+    }
+
+    insert_initial_state(initial_state, mol_entries, folder + '/rn.sqlite')
+
+    subprocess.run([
+        'RNMC',
+        '--database=' + folder + '/rn.sqlite',
+        '--number_of_simulations=1000',
+        '--base_seed=1000',
+        '--thread_count=8',
+        '--step_cutoff=200'])
+
+
+    network_loader = NetworkLoader(
+        folder + '/rn.sqlite',
+        folder + '/ronald_mol_entries.pickle'
+        )
+
 
     report_generator = ReportGenerator(
         network_loader.mol_entries,
-        './scratch/dummy.tex',
+        folder + '/dummy.tex',
         rebuild_mol_pictures=True)
 
     reaction_tally_report(
         network_loader,
-        './scratch/reaction_tally.tex'
+        folder + '/reaction_tally.tex'
     )
 
     LEDC_id = find_mol_entry_from_xyz_and_charge(
@@ -56,17 +98,35 @@ def test_mc_pathfinding(network_loader):
     pathfinding = Pathfinding(network_loader)
     pathfinding.generate_pathway_report(
         LEDC_id,
-        './scratch/LEDC_pathways.tex',
+        folder + '/LEDC_pathways.tex',
         sort_by_frequency=False
     )
 
-    species_report(network_loader, './scratch/species_report.tex')
-    sink_report(network_loader, './scratch/sink_report.tex')
+    species_report(network_loader, folder + '/species_report.tex')
+    sink_report(network_loader, folder + '/sink_report.tex')
     consumption_report(network_loader,
                        LEDC_id,
-                       './scratch/LEDC_consumption_report.tex')
+                       folder + '/LEDC_consumption_report.tex')
 
 
-test_mc_pathfinding(network_loader)
-test_species_filter(network_loader)
-test_reaction_gen(network_loader)
+    if network_loader.number_of_species == 197:
+        print(bcolors.PASS +
+              "li_test: correct number of species" +
+              bcolors.ENDC)
+    else:
+        print(bcolors.FAIL +
+              "li_test: correct number of species" +
+              bcolors.ENDC)
+
+
+
+    if network_loader.number_of_reactions == 5525:
+        print(bcolors.PASS +
+              "li_test: correct number of reactions" +
+              bcolors.ENDC)
+    else:
+        print(bcolors.FAIL +
+              "li_test: correct number of reactions" +
+              bcolors.ENDC)
+
+li_test()
