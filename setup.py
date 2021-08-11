2566b7aaf5778975013dc7cf1607bca13a0f2301
Daniel Barter <danielbarter@gmail.com>
2021-08-11 13:13:03 -0700

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='HiPRGen',
+      version='0.1',
+      description='HiPRGen',
+      url='https://github.com/BlauGroup/HiPRGen',
+      author='Daniel Barter',
+      author_email='danielbarter@gmail.com',
+      license='MIT',
+      packages=['HiPRGen']
+)
