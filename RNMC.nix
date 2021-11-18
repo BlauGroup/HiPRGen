e2640290d74bbb6278f376931295df6ccec3e8e7
Daniel Barter <danielbarter@gmail.com>
2021-11-18 15:09:41 -0800

    updating RNMC

--- RNMC.nix
+++ RNMC.nix
@@ -2,11 +2,11 @@ with (import <nixpkgs> {});
 
 stdenv.mkDerivation rec {
   name = "RNMC";
-  hash = "e1851452407555fc1385eea27a55337164b46813";
+  hash = "4b51b8819470d028d7d43713c9609f54862f0a11";
   buildInputs = [gsl gcc sqlite];
   src = fetchurl {
     url = "https://github.com/BlauGroup/RNMC/archive/${hash}.tar.gz";
-    sha256 = "0bk8bz0hx0w6gpw29iq8h5kxpg1y5qipvsylyi2b7jbwv20zfgmv";
+    sha256 = "sha256-yn8RClfTivrwb+rCvZoEo2FVgobrouBo4Pl2wPZvaqc=";
   };
   buildPhase = ''CC="g++ -O3" ./build.sh'';
   installPhase = "mkdir $out && mkdir $out/bin && mv ./build/GMC $out/bin";
