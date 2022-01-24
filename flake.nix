62a2b43f3ad476f8bf041ec6cec7cbda10eb40db
Daniel Barter <danielbarter@gmail.com>
2022-01-24 09:35:49 -0800

    removing M1 stuff from flake

--- flake.nix
+++ flake.nix
@@ -62,19 +62,16 @@
       devShell = {
         x86_64-linux = genericDevShell "x86_64-linux" false;
         x86_64-darwin = genericDevShell "x86_64-darwin" false;
-        aarch64-darwin = genericDevShell "aarch64-darwin" false;
       };
 
       defaultPackage = {
         x86_64-linux = HiPRGen "x86_64-linux";
         x86_64-darwin = HiPRGen "x86_64-darwin";
-        aarch64-darwin = HiPRGen "aarch64-darwin";
       };
 
       checks = {
         x86_64-linux.tests = HiPRGen "x86_64-linux";
         x86_64-darwin.tests = HiPRGen "x86_64-darwin";
-        aarch64-darwin = HiPRGen "aarch64-darwin";
       };
     };
 
