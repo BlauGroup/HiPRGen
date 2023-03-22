858fbfe10b3df2c717782c6ca02f0a5d65e86e15
jacob-milton <jrmilton@lbl.gov>
2023-03-22 11:45:31 -0700

    commenting out steric filter

--- HiPRGen/reaction_questions.py
+++ HiPRGen/reaction_questions.py
@@ -1136,7 +1136,7 @@ euvl_phase2_reaction_decision_tree = [
     (reaction_is_charge_transfer(), Terminal.KEEP),
     (reaction_is_covalent_decomposable(), Terminal.DISCARD),
     (star_count_diff_above_threshold(6), Terminal.DISCARD),
-    (reaction_is_hindered, Terminal.DISCARD)
+    #(reaction_is_hindered, Terminal.DISCARD)
     (
         fragment_matching_found(),
         [
