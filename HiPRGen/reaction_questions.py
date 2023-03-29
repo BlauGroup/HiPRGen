132a53ed0c5ae5eced43247dbfb076928966937f
jacob-milton <jrmilton@lbl.gov>
2023-03-29 13:05:19 -0700

    found elements

--- HiPRGen/reaction_questions.py
+++ HiPRGen/reaction_questions.py
@@ -1029,7 +1029,7 @@ class reaction_is_hindered(MSONable):
                 neighbor_list = hot_reactant.mol_graph.get_connected_sites(atom)
                 for neighbor in neighbor_list:
                     neighbor_index = neighbor[2]
-                    node_list = hot_reactant.mol_graph.as_dict()['molecule']['nodes']
+                    node_list = hot_reactant.mol_graph.as_dict()['graphs']['nodes']
                     print(node_list)
                     specie = node_list[neighbor_index]['specie']
                     print(specie)
