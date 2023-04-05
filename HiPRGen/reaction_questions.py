f42df0375b7a35d528a2ad339eedbbab1a11f676
jacob-milton <jrmilton@lbl.gov>
2023-04-05 11:27:14 -0700

    Testing H trans

--- HiPRGen/reaction_questions.py
+++ HiPRGen/reaction_questions.py
@@ -538,7 +538,7 @@ class reaction_is_charge_transfer(MSONable):
         return False
 
 
-class reaction_is_covalent_decomposable(MSONable): #removes electron transfers?
+class reaction_is_covalent_decomposable(MSONable): #removes electron transfers and A+B->A+C reactions
     def __init__(self):
         pass
 
@@ -1371,8 +1371,8 @@ euvl_phase1_reaction_logging_tree = [
             (two_closed_shell_reactants_and_two_open_shell_products(), Terminal.DISCARD),
             (reaction_is_charge_separation(), Terminal.DISCARD),
             (reaction_is_covalent_decomposable(), Terminal.DISCARD),
-            (star_count_diff_above_threshold(6), Terminal.KEEP),
-            (compositions_preclude_h_transfer(), Terminal.DISCARD),
+            (star_count_diff_above_threshold(6), Terminal.DISCARD),
+            (compositions_preclude_h_transfer(), Terminal.KEEP),
             (
                 fragment_matching_found(),
                 [
