bcb3371677f12305c3d44b8a21941464793fea62
Hyelim Yang <hyelimyang@78-4f-43-a1-77-ed.dhcp.lbnl.us>
2023-07-25 13:43:00 -0700

    fix error

--- HiPRGen/rxn_networks_graph.py
+++ HiPRGen/rxn_networks_graph.py
@@ -261,7 +261,7 @@ class rxn_networks_graph:
 
         #### Write LMDB ####
         #1 load lmdb
-        lmdb_path = self.report_file_path + "/training.lmdb"
+        lmdb_path = "training.lmdb"
         current_lmdb = LmdbDataset({"src": lmdb_path})
 
         #2 define a dict used to update lmdb. can be initialized by zero
